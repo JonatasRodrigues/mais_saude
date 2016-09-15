bd96bae5fa74870ba52fb51527d64c279125b85d
Jefferson Araujo <araujo.jeffersonp@gmail.com>
2016-09-14 23:14:49 -0300

    Implementaçao parcial da funcionalidade Consulta Medicamentos

---
+++ app/src/main/java/br/com/civico/mais/saude/controle/MedicamentoActivity.java
@@ -0,0 +1,115 @@
+package br.com.civico.mais.saude.controle;
+
+import android.app.Activity;
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.graphics.drawable.ColorDrawable;
+import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ExpandableListAdapter;
+import android.widget.ExpandableListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import org.json.JSONException;
+
+import java.util.concurrent.ExecutionException;
+
+import br.com.civico.mais.saude.R;
+import br.com.civico.mais.saude.adapter.ExpandableListUnidadeAdapter;
+import br.com.civico.mais.saude.dto.ExpandableDTO;
+import br.com.civico.mais.saude.exception.ErroServicoTCUException;
+import br.com.civico.mais.saude.servico.MedicamentoService;
+
+public class MedicamentoActivity extends Activity {
+    ExpandableListView expListView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.unidade_consulta);
+
+        try {
+            ExpandableDTO dto = MedicamentoService.getInstance().consumirServicoTCU();
+            expListView = (ExpandableListView) findViewById(R.id.unidadeListView);
+
+            ExpandableListAdapter listAdapter = new ExpandableListUnidadeAdapter(this, dto.getListDataHeader(),
+                    dto.getListDataChild());
+
+            configurarExpList();
+            expListView.setAdapter(listAdapter);
+        } catch (ErroServicoTCUException e) {
+            exibirMensagemErro(e.getMessage());
+            voltarMenu();
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void exibirMensagemErro(String mensagem){
+        LayoutInflater inflater = getLayoutInflater();
+        View layout = inflater.inflate(R.layout.toast_erro,(ViewGroup) findViewById(R.id.layout_erro));
+        TextView text = (TextView) layout.findViewById(R.id.textErro);
+        text.setText(mensagem);
+
+        final Toast toast = new Toast(getApplicationContext());
+        toast.setGravity(Gravity.CENTER_VERTICAL, 0, 0);
+        toast.setDuration(Toast.LENGTH_LONG);
+        toast.setView(layout);
+
+        //Configurar tempo de exibição
+        int toastDurationInMilliSeconds = 10000;
+        CountDownTimer toastCountDown = new CountDownTimer(toastDurationInMilliSeconds, 1 /*Tick duration*/) {
+            public void onTick(long millisUntilFinished) {
+                toast.show();
+            }
+            public void onFinish() {
+                toast.cancel();
+            }
+        };
+
+        // Show the toast and starts the countdown
+        // toast.show();
+        toastCountDown.start();
+
+    }
+
+    private void voltarMenu(){
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+    }
+
+    private void configurarExpList(){
+        ColorDrawable linhaColor = new ColorDrawable(this.getResources().getColor(R.color.lime));
+        expListView.setChildDivider(getResources().getDrawable(R.color.transparente));
+        expListView.setDivider(linhaColor);
+        expListView.setDividerHeight(2);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        //    if (id == R.id.action_settings) {
+        //        return true;
+        //    }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
